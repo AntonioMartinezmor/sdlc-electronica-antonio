0c41df58edfd3415e44cd9f6c3bd9ce8e1767ed9
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-25 20:41:18 -0600

    se movieron los documentos para cumplir con entrega

--- semana2/ps/src/__init__.py
+++
@@ -1,4 +0,0 @@
-# Este archivo fue recomendado agregar por un asistente IA, debido a que nos 
-# acercamos mas a programacion mas profesional, este archivo es usado para 
-# referenciar que en la carpeta exiten archivos usados para trabajar de manera
-# modular en el testeo de funciones de prueba por medio de TDD. 
