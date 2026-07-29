68aa7529eff1d8713ca1049c9879c474fb2cb2d9
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-28 23:26:09 -0600

    Se actualizo para dar lugar al trabajo del dia martes

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ from app.routers import health , readings
 from app.db import Base, engine
 from app.models import reading 
 
-Base.metadata.create_all(bind=engine)# se crea la tabla para readings 
+Base.metadata.create_all(bind=engine)# se crea la tabla para cualquier modelo 
 #se crean fisicamente en sensorhub con ayuda de bind=engine
 
 app = FastAPI(title="SensorHub")# es encargado de generar la aplicacion 
 # con el nombre de sensorhub
 
-app.include_router(health.router)# arranca con los servidores que nosotros 
-app.include_router(readings.router)# construimos en routers 
+app.include_router(health.router)# registra los servidores que nosotros 
+app.include_router(readings.router)# construimos en routers para su posterior
+# arranque con uvicorn
