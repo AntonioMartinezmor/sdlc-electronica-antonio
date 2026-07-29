f0f66deb05ec4c1f0727d44c6717cde6b97bcbd6
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-29 01:11:25 -0600

    se creo la carpeta para guardar los test

---
+++ tests/test_health.py
@@ -0,0 +1,11 @@
+# se creo el archivo para probar pytest usando un cliente 
+# flotante para probar el codigo
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status":"ok"}
