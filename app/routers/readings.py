2f4a3e570c85226ed3af9e81a32c68e93114d8b7
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-30 00:58:59 -0600

    se actualizo el router para delegar trabajo

--- app/routers/readings.py
+++ app/routers/readings.py
@@ -1,5 +1,6 @@
-#este es el segundo endpoint creado; busca tener relacion con el  
-#pasada con sensores, logrando lectura de datos  a manera de lectura 
+#La funcion dentro de este router cambio, para dar paso al tabajo en capas es decir
+#delega trabajo a otras instancias para no manejar la base de datos desde aqui
+#recurriendo ahora a principios SOLID
 from fastapi import APIRouter
 from app.schemas.reading import ReadingCreate
 from app.repositories.reading_repository import ReadingRepository
@@ -11,7 +12,9 @@ router= APIRouter()#creamos un servidor (mini) para utilizar un endpoint
 @router.post("/readings")#establece el uso de este router para que responda a peticiones post
 def create_reading(reading: ReadingCreate): # validacion de JSON y entrega de datos como
     #objeto python 
-    db = SessionLocal()#inicia la comunicacion con la base de datos
-    repository = ReadingRepository(db)
-    service = ReadingService(repository)
-    return service.create_reading(reading)
+    db = SessionLocal()#inicia la comunicacion con la base de datos/ no para usarse aqui
+    #pero si para otras instancias 
+    repository = ReadingRepository(db) #se crea un objeto readingrepository y se entrega db 
+    service = ReadingService(repository)# se crea otro objeto al que se le entrega el repository
+    return service.create_reading(reading) # envia al servicio los datos validados, regresando 
+    # que servicio devuelve
