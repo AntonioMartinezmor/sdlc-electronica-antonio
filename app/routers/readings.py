8a602c507c0e1caa5443a742efaa8d6145d84acc
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-30 00:46:00 -0600

    se modifico para dar lugar al trabajo del martes

--- app/routers/readings.py
+++ app/routers/readings.py
@@ -2,7 +2,8 @@
 #pasada con sensores, logrando lectura de datos  a manera de lectura 
 from fastapi import APIRouter
 from app.schemas.reading import ReadingCreate
-from app.models.reading import Reading
+from app.repositories.reading_repository import ReadingRepository
+from app.services.reading_service import ReadingService
 from app.db import SessionLocal # aqui tenemos la aparicion del creador de sesiones
 
 router= APIRouter()#creamos un servidor (mini) para utilizar un endpoint
@@ -11,20 +12,6 @@ router= APIRouter()#creamos un servidor (mini) para utilizar un endpoint
 def create_reading(reading: ReadingCreate): # validacion de JSON y entrega de datos como
     #objeto python 
     db = SessionLocal()#inicia la comunicacion con la base de datos
-    try: #a traves de try ejecutamos parte de codigo que sin importa funcione o no 
-        #finally al final se ejecuta; dentro de try encontramos la traduccion de los 
-        #datos recibidos 
-        new_reading = Reading(
-            sensor_id = reading.sensor_id,
-            value = reading.value,
-            unit = reading.unit,
-        )
-        db.add(new_reading)#prepara los datos para ser guardados 
-        db.commit()#se escribe la informacion
-        db.refresh(new_reading)# se actualiza lainformacion en memoria para
-        # dar lugar al id asignado al dato
-        return {"id": new_reading.id, "sensor_id": new_reading.sensor_id,
-                "value": new_reading.value, "unit": new_reading.unit} # se regreasa la id como
-                #prueba del exito del sistema
-    finally:
-        db.close()#se cierra sesion sin importar si haya funcionado o no
+    repository = ReadingRepository(db)
+    service = ReadingService(repository)
+    return service.create_reading(reading)
