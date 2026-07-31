ab9b4786fdf5cdf70d64b09c3d1d03e0ba56a40f
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-31 01:17:55 -0600

    se actualizo el documento para agregar la columna de fecha y hora

--- app/models/reading.py
+++ app/models/reading.py
@@ -1,5 +1,6 @@
 #este documento genera la estructura de la tabla de para la 
-#base de datos para reading de datos 
+#base de datos, reading de datos. Ahora con tiempo incluido
+from datetime import datetime, timezone 
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db import Base 
 
@@ -7,8 +8,13 @@ class Reading(Base):
     __tablename__="readings" # le damos un nombre a la tabla 
     # donde se guardan los datos / a la base de datos 
 
-    id: Mapped[int] = mapped_column(primary_key=True)# este es el identificador 
-    # para las filas siendo un identificador 
+    id: Mapped[int] = mapped_column(primary_key=True)# identificador unico para 
+    #cada fila
     sensor_id: Mapped[str]
     value: Mapped[float]
     unit: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(#en esta version creamos
+    #una columna que guarda automaticamente la fecha y hora de creacion de la fila
+        default= lambda: datetime.now(timezone.utc) #la pieza clave la encontramos
+        # en timezone.utc que corresponde a tiempo universal, sin zona horaria local
+    )
