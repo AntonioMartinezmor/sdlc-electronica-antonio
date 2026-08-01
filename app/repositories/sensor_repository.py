d98fabd3ffe8ac0830a1dec44ac415dded682f42
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-08-01 01:04:22 -0600

    se desarrollo el repositorio para el sensor

---
+++ app/repositories/sensor_repository.py
@@ -0,0 +1,35 @@
+from typing import Optional 
+from sqlalchemy import select
+from sqlalchemy.orm import Session 
+from app.models.sensor import Sensor
+from app.schemas.sensor import SensorCreate
+
+class SensorRepository:
+    def __init__(self,db:Session):
+        self.db=db
+
+    def create(self,sensor:Sensor)->Sensor: 
+        self.db.add(sensor)
+        self.db.commit()
+        self.db.refresh(sensor)
+        return sensor 
+
+    def get_all(self,limit:int=10, offset: int=0) -> list[Sensor]:
+        stmt = select(Sensor).limit(limit).offset(offset)
+        return list(self.db.scalars(stmt))
+
+    def get_by_id(self,sensor_id: int) -> Optional[Sensor]:
+        return self.db.get(Sensor, sensor_id)
+
+    def update(self,sensor: Sensor, data:SensorCreate) -> Sensor: 
+        sensor.name = data.name
+        sensor.sensor_type = data.sensor_type
+        sensor.location = data.location 
+        self.db.commit()
+        self.db.refresh(sensor)
+        return sensor
+
+    def delete(self,sensor: Sensor) ->None:
+        self.db.delete(sensor)
+        self.db.commit()
+        
