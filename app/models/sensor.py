eac3c757c708ba0c7c3db6e0c74cea7662521a28
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-08-01 01:00:05 -0600

    se arreglo sintaxis

--- app/models/sensor.py
+++ app/models/sensor.py
@@ -1,12 +1,12 @@
 #sentamos las bases para la BD, definiendo los 
 #datos en las columnas
-from sqlalchemy.orm import Mapped, mapped_colum
+from sqlalchemy.orm import Mapped, mapped_column
 from app.db import Base
 
 class Sensor(Base):
     __tablename__="sensor"#le damos el nombre de sensor 
 
-    id: Mapped[int] = mapped_colum(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     sensor_type: Mapped[str]
     location: Mapped[str]
