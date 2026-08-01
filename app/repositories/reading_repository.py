8e98d2bc0e04a0e62a4149e7920bb69830a72b0c
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-07-31 18:41:26 -0600

    se actualizo reading repository agregando filtros para paginacion

--- app/repositories/reading_repository.py
+++ app/repositories/reading_repository.py
@@ -1,6 +1,10 @@
 #Este archivo divide el trabajo que en readings se empleo para 
 #guardar los datos que se reciban, buscando respetar los
-# principios solid aprendidos en la primera semana.
+# principios solid aprendidos en la primera semana. Ademas se agregan filtros 
+# para paginacion de las lecturas.
+from datetime import datetime 
+from typing import Optional
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.models.reading import Reading
 
@@ -16,5 +20,30 @@ class ReadingRepository: # definimos una clase molde, para base de datos
         self.db.refresh(reading)#se actualizan los datos de reading a los que se 
         # registraron en la base de datos 
         return reading #devuelve el reading recibido y guardado, tambien el ID 
+
+     #definimos el siguente metodo a manera de filtro  
+    def get_all(
+        self,
+        limit: int= 10, #valores determinados de respuestas 
+        offset: int = 0, #ofrecidas
+        start_date: Optional[datetime] = None, #de manera opcional se recibe la fecha 
+        end_date: Optional[datetime] = None, # en la que se pretende buscar los datos
+    ) ->list[Reading]: # el metodo nos devuelve una lista de las lecturas 
+        stmt = select(Reading)  # es un constructor de consultas que por ahora define aun nada.
     
+
+        if start_date is not None:  #esta es una condicion de busqueda, si nos 
+            #dieron fecha aqui inicia la busqueda 
+            stmt = stmt.where(Reading.created_at >= start_date)
+
+        if end_date is not None:  #lo mismo que el de arriba pero con el final 
+         #indice de busqueda
+            stmt = stmt.where(Reading.created_at <= end_date)
+
+        stmt = stmt.limit(limit).offset(offset)  #en esta linea se agrega la paginacion, ya 
+        # con los limites impuestos 
+
+        return list(self.db.scalars(stmt)) # por ultimo ejecutamos la consulta
+        # contra la base de datos
+
 
