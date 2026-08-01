5f9507495fe5fbca2422cb40f4f6f711f8df9044
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-08-01 00:28:45 -0600

    desarrollamos la estructura de la base de datos del nuevo sensor

---
+++ app/schemas/sensor.py
@@ -0,0 +1,9 @@
+#desarrollamos este nuevo sensor como identidad propia 
+#aprovechando que ya creamos diferentes bases 
+#orientadas a los sensores ya utilizados
+from pydantic import BaseModel
+
+class SensorCreate(BaseModel): 
+    name:str
+    sensor_type: str
+    location: str
