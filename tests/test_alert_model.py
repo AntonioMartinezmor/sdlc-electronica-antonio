8cb1499d823841afe16a5b6375605b99762a28eb
Antonio Martinez <martinezmoralesl201@gmail.com>
2026-08-20 01:07:20 -0600

    se arreglo estrucutra y sintaxis del test (problemas con sqlalchemy y open

--- tests/test_alert_model.py
+++ tests/test_alert_model.py
@@ -1,5 +1,18 @@
 from app.models.alert import Alert
-def test_alert_CPD():
-    alert = Alert(sensor_id="SENSOR_01",message="Valor fuera de rango")
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.db import Base
+
+def test_alert_OCPD():
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    db = Session()
+
+    alert = Alert(sensor_id="SENSOR_01", reading_id=1, message="Valor fuera de rango")
+    db.add(alert)
+    db.commit()
+    db.refresh(alert)
+
     assert alert.status == "open"
     
